5a2fd8179a2cda294a459c35719f889c785db12f
shuvomdj <shuvomdj@gmail.com>
2025-11-15 22:58:43 +0900

    Update BasicCalculatorV1.java

--- BasicCalculatorV1.java
+++ BasicCalculatorV1.java
@@ -3,15 +3,15 @@ package twentyfive.fall.oop.group2.lesson1.class3;
 import java.util.Scanner;
 public class basiccalculator {
     public static void main(String[] args) {
-        // Input first entry
+        // Input first entry value
         System.out.println("First Number Input: ");
         Scanner to = new Scanner(System.in); // input first data to scanner
         float toadd = to.nextFloat();
-        // Input math mode
+        // Input math mode (+, -, *, /)
         System.out.println("Input Math Function (+, -, *, /): ");
         Scanner motiv = new Scanner(System.in);
         char motivadd = motiv.next().charAt(0);
-        // input 2nd entry
+        // input 2nd entry value
         System.out.println("Second No Input: ");
         Scanner with = new Scanner(System.in);
         float withadd = with.nextFloat();
